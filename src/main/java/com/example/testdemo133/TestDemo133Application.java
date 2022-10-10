1687a0790ac38e244be365010114c94d7dead75d
seferovramin7 <seferovramin7@gmail.com>
2022-10-10 19:57:15 +0400

    1. Etdiyim Deyishiklikler

---
+++ src/main/java/com/example/testdemo133/TestDemo133Application.java
@@ -0,0 +1,13 @@
+package com.example.testdemo133;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDemo133Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDemo133Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testdemo133/TestDemo133ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testdemo133;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDemo133ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
